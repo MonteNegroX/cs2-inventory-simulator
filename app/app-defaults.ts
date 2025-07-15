babf09d9544a4828ebd49c7df3635f8ce7c763c0
testing <forester.eremenko@gmail.com>
2025-07-15 21:06:08 +0400

    fix

--- app/app-defaults.ts
+++ app/app-defaults.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export const DEFAULT_APP_NAME = "CS2 Inventory Simulator";
+export const DEFAULT_APP_NAME = "CAPSULE";
 export const DEFAULT_APP_FOOTER_NAME = "cstrike's Inventory Simulator";
 export const DEFAULT_APP_DESCRIPTION = `Counter-Strike 2 Inventory Simulator. Craft items, open cases, and apply stickers - organize and plan your dream inventory.`;
 export const DEFAULT_APP_TITLE = `The best free, Counter-Strike 2 Inventory Simulator`;
